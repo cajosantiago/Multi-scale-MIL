4a51c1f10623fbf4c2c523549514a7e2748145d6
cajosantiago <cajosantiago@gmail.com>
2025-10-08 13:00:41 +0100

    updated gradio demo

--- gradio_demo.py
+++ gradio_demo.py
@@ -17,6 +17,7 @@ import tarfile
 import requests
 from PIL import Image
 import gradio as gr
+from scipy.ndimage import gaussian_filter, binary_erosion, binary_dilation, median_filter, gaussian_filter
 
 os.environ['GRADIO_SERVER_NAME'] = "0.0.0.0"
 
